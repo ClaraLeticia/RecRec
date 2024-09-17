ef928d0b3f5ded0ae41a01dd7408b04f81f2170e
lauramoroni <moronilaura28@gmail.com>
2024-09-17 08:54:48 -0300

    calculo delta menor que zero

--- calc/quadratic.py
+++ calc/quadratic.py
@@ -1,10 +1,20 @@
-from math import sqrt
+from math import atan, sqrt, degrees
+from operator import neg
 
 def quadratic(a, b, c )-> dict:
     b = b/a
     c = c/a
     print(a, b, c, b**2 - 4 * a * c)
     d = b**2 - 4 * a * c
-    r1 = (-b + sqrt(d)) / 2 * a
-    r2 = (-b - sqrt(d)) / 2 * a
-    return {'r1':r1, 'r2': r2, 'delta': d }
+    if d >= 0:   
+        r1 = (-b + sqrt(d)) / 2 * a
+        r2 = (-b - sqrt(d)) / 2 * a
+        p = None
+        alpha = None
+    else:
+        r1 = complex(-b / (2 * a), sqrt(abs(d)) / (2 * a))
+        r2 = complex(-b / (2 * a), neg(sqrt(abs(d)) / (2 * a)))
+
+        p = round(sqrt(pow(sqrt(abs(d)) / (2 * a), 2) + pow((-b / (2 * a)), 2)))
+        alpha = round(degrees(atan((sqrt(abs(d)) / (2 * a)) / (-b / (2 * a)))))
+    return {'r1': round(r1,2), 'r2': round(r2,2), 'delta': d, 'p': p, 'alpha':alpha}
